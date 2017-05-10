13c84fb4c096a38ba917f4db863889eaae22a525
Nikita K <vrolok15@yandex.ru>
2017-05-10 03:55:15 +0300

    Added age check for subscriptions, fixed Membership Type magic numbers

---
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer) validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == MembershipType.Unknown || 
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
+            { 
+                return ValidationResult.Success;
+            }
+
+            if (customer.Birthdate == null)
+            {
+                return new ValidationResult("Birthdate is required for a Subscription Membership");
+            }
+
+            var today = DateTime.Today;
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            if (customer.Birthdate.Value > today.AddYears(-age)) age--;
+
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 for a Subscription Membership");
+        }
+    }
+}
